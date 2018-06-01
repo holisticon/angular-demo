0dc15ef2b50a06a4f8ec82dca4eb0658c5550ff0
Marvin Luchs <marvin.luchs@t-systems.com>
2018-06-01 19:57:39 +0200

    feat: added orders view

--- libs/orders/src/orders.module.ts
+++ libs/orders/src/orders.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule.forChild([
-            /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+            { path: '', pathMatch: 'full', component: OrdersComponent }
         ])
+    ],
+    declarations: [
+        OrdersComponent
     ]
 })
 export class OrdersModule {}
---
+++ libs/orders/src/orders/orders.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'cfha-orders',
+    templateUrl: './orders.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class OrdersComponent {
+
+}
