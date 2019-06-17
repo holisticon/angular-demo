1a503b22c627a8f7a723b1467d0942193da2edfb
Marvin Luchs <marvin.luchs@holisticon.de>
2019-06-17 16:17:16 +0200

    fix: 🐛 Module not found: Error: Can't resolve index.ngfactory

--- libs/homepage/src/index.ts
+++ libs/homepage/src/index.ts
@@ -1,1 +1,4 @@
-export { HomepageModule } from './lib/homepage.module';
+// use * instead of named export as fix for "Module not found: Error: Can't resolve '[...]/index.ngfactory'"
+// see https://github.com/angular/angular-cli/issues/14707s
+export * from './lib/homepage.module';
+
--- libs/products/src/index.ts
+++ libs/products/src/index.ts
@@ -1,2 +1,5 @@
-export { ProductsModule } from './lib/products.module';
-export * from './lib/product-list/product-list.component'
+// use * instead of named export as fix for "Module not found: Error: Can't resolve '[...]/index.ngfactory'"
+// see https://github.com/angular/angular-cli/issues/14707s
+export * from './lib/product-list/product-list.component';
+export * from './lib/products.module';
+
