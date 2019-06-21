870935636cc4c0a6b7cc0b7f385730c99fa91b36
Marvin Luchs <marvin.luchs@holisticon.de>
2019-06-21 19:04:00 +0200

    fix: remove marbles fix

--- test.ts
+++ test.ts
@@ -1,26 +1,16 @@
 
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-import { addMatchers, initTestScheduler, getTestScheduler, resetTestScheduler } from 'jasmine-marbles';
 
 declare const require: any;
-
-// fix for "No test scheduler initialized" after updating to Angular 8
-// see https://github.com/synapse-wireless-labs/jasmine-marbles/issues/40#issuecomment-499785967
-const env = jasmine.getEnv();
-
-env.beforeAll(() => addMatchers());
-env.beforeEach(() => initTestScheduler());
-env.afterEach(() => {
-    getTestScheduler().flush();
-    resetTestScheduler();
-});
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
@@ -30,4 +20,5 @@ getTestBed().initTestEnvironment(
 // Then we find all the tests.
 const context = require.context('./', true, /^\.\/(apps|libs)\/.+\.spec\.ts$/);
 // And load the modules.
-context.keys().map(context);
+context.keys().map(context);    
+    
