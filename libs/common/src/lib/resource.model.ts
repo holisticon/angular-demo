77dc3b0abb88753b52bc49d878168f34684ada95
Marvin Luchs <marvin.luchs@holisticon.de>
2019-07-09 08:32:42 +0200

    refactor: 💡 ResourceId represents the ID of a resource

--- libs/common/src/lib/resource.model.ts
+++ libs/common/src/lib/resource.model.ts
@@ -1,8 +1,6 @@
-export type Resource<T> = T & ResourceId;
+export type Resource<T> = T & { _id: ResourceId };
 
-export interface ResourceId {
-    _id: string;
-}
+export type ResourceId = string;
 
 export interface ResourceWith<T, R extends Resource<any> = Resource<any>> {
     resource: R,
--- libs/common/src/test/resource.utils.ts
+++ libs/common/src/test/resource.utils.ts
@@ -1,6 +1,6 @@
-import { ResourceId, Resource } from '@ngxp/common';
+import { Resource, ResourceId } from '@ngxp/common';
 
-export function addId<T extends object>(obj: T, id: ResourceId['_id']): Resource<T> {
+export function addId<T extends object>(obj: T, id: ResourceId): Resource<T> {
     return {
         _id: id,
         ...(<any> obj)
