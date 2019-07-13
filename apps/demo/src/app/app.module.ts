0c3ad2be4be0e8a68e65a812c6c9fa3ae1ec4ea2
Marvin Luchs <marvin@luchs.org>
2019-07-13 13:39:03 +0200

    fix: unnecessary homepage module import

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -7,19 +7,15 @@ import { EffectsModule } from '@ngrx/effects';
 import { routerReducer, RouterState, StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { HomepageModule } from '@ngxp/homepage';
 import { storeLogger } from 'ngrx-store-logger';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppEffects } from './state/app.effects';
-
-
 @NgModule({
     imports: [
         BrowserModule,
         HttpClientModule, // see https://github.com/angular/angular-cli/issues/10170,
-        HomepageModule,
         RouterModule.forRoot([
             { path: '', loadChildren: () => import('@ngxp/homepage').then(m => m.HomepageModule) },
             { path: 'products', loadChildren: () => import('@ngxp/products').then(m => m.ProductsModule) },
