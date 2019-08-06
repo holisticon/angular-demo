25b1dd33583552067b114d69f6f9b7b5bc2edafe
Marvin Luchs <marvin.luchs@eventim.de>
2019-08-06 17:37:40 +0200

    refactor: use AddToShoppinCartForm component in product list

--- libs/products/src/lib/search-results/search-results.component.spec.ts
+++ libs/products/src/lib/search-results/search-results.component.spec.ts
@@ -1,11 +1,8 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
 import { expectElementFromFixture } from '@ngxp/common/test';
-import { product, products } from '@ngxp/products-common/test';
-import { getId } from '@ngxp/resource';
-import { AdditionToShoppingCart, ShoppingCartCommonStore } from '@ngxp/shopping-cart-common';
+import { products } from '@ngxp/products-common/test';
 import { provideStoreServiceMock } from '@ngxp/store-service/testing';
 import { ProductListComponent } from '../product-list/product-list.component';
 import { ProductsStore } from '../state/products-store.service';
@@ -14,7 +11,6 @@ import { SearchResultsComponent } from './search-results.component';
 describe('SearchResultsComponent', () => {
     let component: SearchResultsComponent;
     let fixture: ComponentFixture<SearchResultsComponent>;
-    let shoppingCartCommonStore: ShoppingCartCommonStore;
 
     const searchResults = products;
 
@@ -29,13 +25,10 @@ describe('SearchResultsComponent', () => {
             providers: [
                 provideStoreServiceMock(ProductsStore, {
                     getSearchResults: searchResults
-                }),
-                provideStoreServiceMock(ShoppingCartCommonStore)
+                })
             ]
         })
             .compileComponents();
-
-        shoppingCartCommonStore = TestBed.get(ShoppingCartCommonStore);
     }));
 
     beforeEach(() => {
@@ -50,17 +43,4 @@ describe('SearchResultsComponent', () => {
         expectElementFromFixture(fixture, 'ngxp-product-list').not.toBeNull();
         expect(productList.products).toEqual(searchResults);
     });
-
-    it('adds the product to the shopping cart when the product list emits an addToShoppingCart event', async(() => {
-        const additionToShoppingCart: AdditionToShoppingCart = {
-            product: getId(product),
-            quantity: 2
-        };
-        const addToShoppingCartSpy = spyOn(shoppingCartCommonStore, 'addToShoppingCart');
-        const productList = fixture.debugElement.query(By.css('ngxp-product-list'));
-
-        productList.triggerEventHandler('addToShoppingCart', additionToShoppingCart);
-
-        expect(addToShoppingCartSpy).toHaveBeenCalledWith({ additionToShoppingCart });
-    }));
 });
--- libs/shopping-cart-common/src/index.ts
+++ libs/shopping-cart-common/src/index.ts
@@ -2,4 +2,3 @@ export { ShoppingCartCommonModule } from './lib/shopping-cart-common.module';
 export * from './lib/shopping-cart.model';
 export * from './lib/shopping-cart.util';
 export * from './lib/state/shopping-cart-common.actions';
-export * from './lib/state/shopping-cart-common-store.service';
