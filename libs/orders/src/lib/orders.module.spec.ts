b1e289e5ad5f89c52046717dee1d24f0c4361f3c
Marvin Luchs <marvin.luchs@eventim.de>
2019-08-06 17:13:15 +0200

    fix: quick fix for dispatching actions on module load

--- libs/orders/src/lib/orders.module.spec.ts
+++ libs/orders/src/lib/orders.module.spec.ts
@@ -1,15 +1,17 @@
-import { async } from '@angular/core/testing';
+import { fakeAsync, tick } from '@angular/core/testing';
 import { createStoreServiceMock } from '@ngxp/store-service/testing';
 import { OrdersModule } from './orders.module';
 import { OrdersStore } from './state/orders-store.service';
 
 describe('OrdersModule', () => {
-    it('loads the orders on initialization', async(() => {
+    it('loads the orders on initialization', fakeAsync(() => {
         const ordersStore = createStoreServiceMock(OrdersStore);
         const loadOrdersSpy = spyOn(ordersStore, 'loadOrders');
 
         // tslint:disable-next-line: no-unused-expression
         new OrdersModule(ordersStore);
+
+        tick();
 
         expect(loadOrdersSpy).toHaveBeenCalled();
     }));
--- libs/user-profile-common/src/lib/user-profile-common.module.spec.ts
+++ libs/user-profile-common/src/lib/user-profile-common.module.spec.ts
@@ -1,15 +1,17 @@
-import { async } from '@angular/core/testing';
+import { fakeAsync, tick } from '@angular/core/testing';
 import { createStoreServiceMock } from '@ngxp/store-service/testing';
 import { UserProfileCommonStore } from './state/user-profile-common-store.service';
 import { UserProfileCommonModule } from './user-profile-common.module';
 
 describe('UserProfileCommonModule', () => {
-    it('dispatches a LoadUserProfileAction on initialization', async(() => {
+    it('dispatches a LoadUserProfileAction on initialization', fakeAsync(() => {
         const userProfileCommonStore = createStoreServiceMock(UserProfileCommonStore);
         const loadUserProfileSpy = spyOn(userProfileCommonStore, 'loadUserProfile');
 
         // tslint:disable-next-line: no-unused-expression
         new UserProfileCommonModule(userProfileCommonStore);
+
+        tick();
 
         expect(loadUserProfileSpy).toHaveBeenCalled();
     }));
