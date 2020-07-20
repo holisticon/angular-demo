06217b26ec21a5de02ec44bf426e604f7ea8f908
Marvin Luchs <marvin.luchs@holisticon.de>
2020-07-20 20:07:50 +0200

    chore: update to Angular 10, Nx 10

--- libs/user-profile/jest.config.js
+++ libs/user-profile/jest.config.js
@@ -1,10 +1,21 @@
 module.exports = {
-  name: 'user-profile',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/user-profile',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js'
-  ]
+    name: 'user-profile',
+    preset: '../../jest.config.js',
+    coverageDirectory: '../../coverage/libs/user-profile',
+    snapshotSerializers: [
+        'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+        'jest-preset-angular/build/AngularSnapshotSerializer.js',
+        'jest-preset-angular/build/HTMLCommentSerializer.js'
+    ],
+    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+    globals: {
+        'ts-jest': {
+            tsConfig: '<rootDir>/tsconfig.spec.json',
+            stringifyContentPathRegex: '\\.(html|svg)$',
+            astTransformers: [
+                'jest-preset-angular/build/InlineFilesTransformer',
+                'jest-preset-angular/build/StripStylesTransformer'
+            ]
+        }
+    }
 };
