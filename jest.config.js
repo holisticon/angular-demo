c785a188898cd1429756a7fd405821da0b9f3058
Marvin Luchs <marvin@luchs.org>
2020-07-31 08:13:32 +0200

    feat: add config for vscode-jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+    testPathIgnorePatterns: ['demo-e2e'],
     transform: {
         '^.+\\.(ts|js|html)$': 'ts-jest'
     },
@@ -9,5 +10,22 @@ module.exports = {
     resolver: '@nrwl/jest/plugins/resolver',
     moduleFileExtensions: ['ts', 'js', 'html'],
     coverageReporters: ['html', 'text'],
-    coveragePathIgnorePatterns: ['test']
+    coveragePathIgnorePatterns: ['test'],
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['./test-setup.ts'],
+    snapshotSerializers: [
+        'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+        'jest-preset-angular/build/AngularSnapshotSerializer.js',
+        'jest-preset-angular/build/HTMLCommentSerializer.js'
+    ],
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.spec.json',
+            stringifyContentPathRegex: '\\.(html|svg)$',
+            astTransformers: [
+                'jest-preset-angular/build/InlineFilesTransformer',
+                'jest-preset-angular/build/StripStylesTransformer'
+            ]
+        }
+    }
 };
--- libs/resource/src/test/index.ts
+++ libs/resource/src/test/index.ts
@@ -1,3 +1,2 @@
 export * from './data/resource.data';
 export * from './resource-builder';
-export * from './resource.utils';
