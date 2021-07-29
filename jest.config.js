4842ccbc0540705e8fe7d383eb9227fec0674532
Marvin Luchs <marvin.luchs@holisticon.de>
2021-07-29 18:07:08 +0200

    chore: update to nx 12.6

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,5 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 module.exports = {
-    testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-    transform: {
-        '^.+\\.(ts|js|html)$': 'ts-jest'
-    },
-    moduleNameMapper: {
-        'lodash-es': 'lodash'
-    },
-    resolver: '@nrwl/jest/plugins/resolver',
-    moduleFileExtensions: ['ts', 'js', 'html'],
-    coverageReporters: ['html', 'text'],
-    coveragePathIgnorePatterns: ['test']
+    "projects": getJestProjects()
 };
